bad94a66fd9f51ab27e3295a7e93328226953ef6
chai <ssumachai@gmail.com>
2022-02-07 18:35:31 -0800

    Created Empty Files

---
+++ Laboratory Assignment 6 - Task Scheduler/Code/ssuks001_lab6_part1.cpp
@@ -0,0 +1,16 @@
+/*         Your Name & E-mail:  Sumachai Suksanguan (ssuks001@ucr.edu)
+*          Lab Section:    021 
+ *         Assignment:    Lab 6  Exercise 1
+ *         Exercise Description: 
+ *        
+ *         I acknowledge all content contained herein, excluding template or example code, is my own original work.
+ *
+ *         Demo Link: 
+ */
+
+void setup()
+{}
+
+void Tick(){}
+
+void loop(){}
